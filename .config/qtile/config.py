e3bf3c1d36c0359e6b3040e05729647125f860ed
Peter Ye <yaxollum@gmail.com>
2022-07-06 23:04:20 -0400

    use Alt instead of Super as mod

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -29,7 +29,7 @@ from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
-mod = "mod4"
+mod = "mod1"
 terminal = guess_terminal()
 
 keys = [
