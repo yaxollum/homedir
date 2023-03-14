ecaf90f0f79c63b9706aaeacc6d6d2275c2fa34b
Peter Ye <yaxollum@gmail.com>
2023-03-14 19:45:58 -0400

    update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,13 +6,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(package-selected-packages '(slime evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
+(setq evil-want-C-u-scroll t)
 (require 'evil)
   (evil-mode 1)
+
+(setq inferior-lisp-program "sbcl")
+(blink-cursor-mode 0)
