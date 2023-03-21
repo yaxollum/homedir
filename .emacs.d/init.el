14a43c3aaad280ce645b0db8c177edf4235d47c6
Peter Ye <yaxollum@gmail.com>
2023-03-21 16:59:25 -0400

    update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,13 +6,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(package-selected-packages '(slime evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 120 :width normal)))))
 (setq evil-want-C-u-scroll t)
 (require 'evil)
   (evil-mode 1)
