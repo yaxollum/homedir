63369292af44964a49948fdbaee6c1c711886566
Peter Ye <yaxollum@gmail.com>
2024-08-02 20:48:03 -0400

    add xset

--- bin/fedora/dnf.sh
+++ bin/fedora/dnf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dnf install flatpak lightdm gvim alacritty i3 google-noto-serif-cjk-ttc-fonts dejavu-sans-mono-fonts
+dnf install flatpak lightdm gvim alacritty i3 google-noto-serif-cjk-ttc-fonts dejavu-sans-mono-fonts xset
 
 dnf groupinstall "Development Tools"
 
