dbc463b8e4212ad94e22f01751cd1023847f044d
Peter Ye <yaxollum@gmail.com>
2025-04-09 22:39:25 -0400

    update virtualbox config

--- bin/virtualbox-dhcp.sh
+++ bin/virtualbox-dhcp.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-VBoxManage dhcpserver modify --interface=vboxnet0 --vm=arch --fixed-address=192.168.56.3
+
+# Configuration is located in ~/.config/VirtualBox/HostInterfaceNetworking-vboxnet0-Dhcpd.config
+# May need to delete existing leases in the .leases file in the same directory as config
+# Need to restart the VirtualBox dhcp server to update config
+
+VBoxManage dhcpserver modify --interface=vboxnet0 --mac-address=<enter mac addr here> --fixed-address=192.168.56.123
